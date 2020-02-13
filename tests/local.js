9bf290513ab666c726e6ce3910f161c37ca6eaaa
luke <lukevance90@gmail.com>
2020-02-12 22:26:48 -0700

    updated quadrant function

--- tests/local.js
+++ tests/local.js
@@ -1,12 +1,38 @@
-import {getCenter} from 'geolib';
+const getCenterOfBounds = require('geolib').getCenterOfBounds;
 
-const highlandsCoordinates = {
-    latitude: "",
-    longitude: ""
+const originalBoundary = {
+    southwest: {
+        latitude: "-105.053995",
+        longitude: "39.742223"
+    },
+    northeast: {
+        latitude: "-105.020048",
+        longitude: "39.785519"
+    }
 };
 
-const center = getCenter([
-    highlandsCoordinates
+const boundaryCenter = getCenterOfBounds([
+    originalBoundary.southwest,
+    originalBoundary.northeast
 ]);
 
-console.log(center);
+const quadrants = {
+    SW: {
+        sw: originalBoundary.southwest,
+        ne: boundaryCenter
+    },
+    NW: {
+        sw: "",
+        ne: ""
+    },
+    NE: {
+        sw: boundaryCenter,
+        ne: originalBoundary.northeast
+    },
+    SE: {
+        sw: "",
+        ne: ""
+    }
+}
+
+console.log(westDenverCenter);
