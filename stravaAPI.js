121aed392d7cd190b045bb2e9799916835556568
luke <lukevance90@gmail.com>
2020-02-17 19:33:48 -0700

    explore api  slash topTen segments method is working

--- stravaAPI.js
+++ stravaAPI.js
@@ -2,14 +2,17 @@ const fetch = require('node-fetch');
 const env = require('./env.json');
 
 module.exports.topTen = async (southwest, northeast) => {
+    console.log(env.STRAVATOKEN);
     const url = "https://www.strava.com/api/v3/segments/explore"
-    const queryParams = `?activity_type=running&bounds=${southwest.latitude}, ${southwest.longitude}, ${northeast.latitude}, ${northeast.longitude}`;
+    const queryParams = `?activity_type=running&bounds=${southwest.latitude},${southwest.longitude},${northeast.latitude},${northeast.longitude}`;
+    const completeURL = url + queryParams;
     const options = {
         headers: {
             Authorization: `Bearer ${env.STRAVATOKEN}`
         }
     };
-    const res = await fetch(`${url}${queryParams}`, options);
+    console.info(completeURL);
+    const res = await fetch(completeURL, options);
     const json = await res.json();
     return json;
 };
